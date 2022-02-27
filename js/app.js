7709881282098e97621af9f04dd5212a1896fef1
Nenad Stojanovic <nenad@distantjob.com>
2022-02-27 21:25:02 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -13,3 +13,28 @@ hamburger_close_btn.addEventListener("click", () => {
     nav_el.classList.remove("nav__visible");
   });
 });
+
+const links = document.querySelectorAll(".navbar-link .btn-dropdown");
+const dropdown = document.querySelectorAll(".navbar-link ul");
+
+dropdown.forEach((element) => {
+  const height = element.offsetHeight;
+  element.style.marginTop = `${-height}px`;
+});
+
+//////////////////////////////Dropdown TEST ////////////////////////////////
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    const target = e.currentTarget;
+    const ele = target.nextElementSibling;
+
+    dropdown.forEach((element) => {
+      if (element !== ele) {
+        element.previousElementSibling.classList.remove("active");
+        element.classList.remove("dropdown");
+      }
+    });
+    target.classList.toggle("active");
+    ele.classList.toggle("dropdown");
+  });
+});
